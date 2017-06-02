eb2db9371d4d5e4aa6a38d51d53e70270c4de6f6
USUARIO <USUARIO@PIPE>
2017-06-02 02:29:24 -0500

    Exceleando

--- src/main/java/com/crunchify/jsp/servlet/HSSFCreate.java
+++ src/main/java/com/crunchify/jsp/servlet/HSSFCreate.java
@@ -14,20 +14,22 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 public class HSSFCreate extends HttpServlet {
+
     public void init(ServletConfig config) throws ServletException {
-        super.init(config);  
+        super.init(config);
     }
 
     public void destroy() {
     }
 
-    /** Processes requests for both HTTP GET and POST methods.
+    /**
+     * Processes requests for both HTTP GET and POST methods.
+     *
      * @param request servlet request
      * @param response servlet response
      */
-
     protected void processRequest(HttpServletRequest request,
-        HttpServletResponse response) throws ServletException, IOException {
+            HttpServletResponse response) throws ServletException, IOException {
         ServiciosDAO dao = new ServiciosDAO();
         response.setContentType("application/vnd.ms-excel");
         HSSFWorkbook wb = new HSSFWorkbook();
@@ -38,66 +40,75 @@ public class HSSFCreate extends HttpServlet {
         Lotes = dao.ArrayLotes();
         ArrayList Prod = new ArrayList();
         Prod = dao.ArrayProd();
-       
+
         Map<String, Object[]> data = new HashMap<String, Object[]>();
-		data.put("1", new Object[] {"", "", "Usuarios", "", "", "","", "Lotes","","","","","Productos"});
-		data.put("2", new Object[] {"", "ID", "", "Nombre","","","Id Lote","","Nom.Lote","","","Id Prod.","Cantidad","Nombre Producto"});
-                
-                
-		data.put("3", new Object[] {"", Users.get(0), "", Users.get(1),"","",Lotes.get(0),"",Lotes.get(1),"","",Prod.get(0),Prod.get(1),Prod.get(2)});
-		
-                
-                data.put("4", new Object[] {3d, "Dean", 700000d});
-		
-		Set<String> keyset = data.keySet();
-		int rownum = 0;
-		for (String key : keyset) {
-			Row row = sheet.createRow(rownum++);
-			Object [] objArr = data.get(key);
-			int cellnum = 0;
-			for (Object obj : objArr) {
-				Cell cell = row.createCell(cellnum++);
-				if(obj instanceof Date) 
-					cell.setCellValue((Date)obj);
-				else if(obj instanceof Boolean)
-					cell.setCellValue((Boolean)obj);
-				else if(obj instanceof String)
-					cell.setCellValue((String)obj);
-				else if(obj instanceof Double)
-					cell.setCellValue((Double)obj);
-			}
-		}
-        
+        data.put("1", new Object[]{"", "", "Usuarios", "", "", "", "", "Lotes", "", "", "", "", "Productos"});
+        data.put("2", new Object[]{"", "ID", "", "Nombre", "", "", "Id Lote", "", "Nom.Lote", "", "", "Id Prod.", "Cantidad", "Nombre Producto"});
+
+        for (int i = 0; i < Prod.size(); i++) {
+            data.put(""+i+"", new Object[]{"", Users.get(i), "", Users.get(i), "", "", Lotes.get(i), "", Lotes.get(i), "", "", Prod.get(i), Prod.get(i), Prod.get(i)});
+        }
+        for (int x = Prod.size(); x < Lotes.size(); x++) {
+            data.put(""+x+"", new Object[]{"", Users.get(x), "", Users.get(x), "", "", Lotes.get(x), "", Lotes.get(x), "", "", "", "", ""});
+        }
+        for (int y = Prod.size()+Lotes.size(); y < Lotes.size(); y++) {
+            data.put(""+y+"", new Object[]{"", Users.get(y), "", Users.get(y), "", "", "", "", "", "", "", "", "", ""});
+        }
+
+        data.put("4", new Object[]{3d, "Dean", 700000d});
+
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof Date) {
+                    cell.setCellValue((Date) obj);
+                } else if (obj instanceof Boolean) {
+                    cell.setCellValue((Boolean) obj);
+                } else if (obj instanceof String) {
+                    cell.setCellValue((String) obj);
+                } else if (obj instanceof Double) {
+                    cell.setCellValue((Double) obj);
+                }
+            }
+        }
+
         // Write the output 
         OutputStream out = response.getOutputStream();
         wb.write(out);
         out.close();
     }
 
-    /** Handles the HTTP <code>GET</code> method.
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      */
-
     protected void doGet(HttpServletRequest request,
-        HttpServletResponse response) throws ServletException, IOException {
+            HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** Handles the HTTP POST method.
+    /**
+     * Handles the HTTP POST method.
+     *
      * @param request servlet request
      * @param response servlet response
      */
-
     protected void doPost(HttpServletRequest request,
-        HttpServletResponse response) throws ServletException, IOException {
+            HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** Returns a short description of the servlet.
+    /**
+     * Returns a short description of the servlet.
      */
-
     public String getServletInfo() {
-       return "Example to create a workbook in a servlet using HSSF";
+        return "Example to create a workbook in a servlet using HSSF";
     }
 }
